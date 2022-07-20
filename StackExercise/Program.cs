192a6adbc76bb6d3dd0456826cc633afc10de92a
Omar Badawi <itsomar278@gmail.com>
2022-07-21 01:51:00 +0300

    first commit : gitignore + hello world

---
+++ StackExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack;
+public class program
+{
+   static void  Main ()
+    {
+        Console.WriteLine("OMAR");
+    
+    }
+  
+}
